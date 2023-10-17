8e992de88456c642221fccfea98ccd6d5cc4be67
Felipe Araujo <fearaujo871@gmail.com>
2023-10-17 16:13:33 -0300

    adicionando rota listar carros

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const knex = require('./conexao')
 
 app.use(express.json());
 
 app.get('/',async (req,res)=>{
-    return res.status(200).json('Tudo Ok!')
+    try {
+        const carros = await knex('carros')
+        return res.status(200).json(carros)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({mensagem: 'erro interno do servidor'})
+    }
 })
 
 const port = process.env.PORT || 3000
